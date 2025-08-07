7edf79df219e77544c9c4cc5371dddc814c121b7
Carol Papi <carolina.papi@jazztech.com.br>
2025-08-07 12:16:36 -0300

    iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('should return 200 for valid credentials', async () => {
+            // Test logic for valid login
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'password': '123456'
+                });
+
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body).to.have.property('token');
+        });
+    });
+});
