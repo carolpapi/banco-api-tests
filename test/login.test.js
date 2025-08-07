e53f3629411c2b108a2eef3fc912f46e1c78059f
Carol Papi <carolina.papi@jazztech.com.br>
2025-08-07 17:43:26 -0300

    Adicionando a biblioteca .env e sendo muito humilhada pelo js meu pai amado para variáveis globais

--- test/login.test.js
+++ test/login.test.js
@@ -1,11 +1,14 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import dotenv from 'dotenv';
+dotenv.config();
+const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
 describe('Login', () => {
     describe('POST /login', () => {
         it('should return 200 for valid credentials', async () => {
             // Test logic for valid login
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(baseUrl)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,5 +1,8 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import dotenv from 'dotenv';
+dotenv.config();
+const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
 describe('Transferências', () => {
     describe('POST /transferencia', () => {
@@ -7,7 +10,7 @@ describe('Transferências', () => {
             'Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00',
             async () => {
                 // capturar o token
-                const respostaLogin = await request('http://localhost:3000')
+                const respostaLogin = await request(baseUrl)
                     .post('/login')
                     .set('Content-Type', 'application/json')
                     .send({
@@ -17,7 +20,7 @@ describe('Transferências', () => {
 
                 const token = respostaLogin.body.token;
 
-                const resposta = await request('http://localhost:3000')
+                const resposta = await request(baseUrl)
                     .post('/transferencia')
                     .set('Content-Type', 'application/json')
                     .set('Authorization', 'Bearer ' + token)
@@ -38,7 +41,7 @@ describe('Transferências', () => {
             'Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00',
             async () => {
                 // capturar o token
-                const respostaLogin = await request('http://localhost:3000')
+                const respostaLogin = await request(baseUrl)
                     .post('/login')
                     .set('Content-Type', 'application/json')
                     .send({
@@ -48,7 +51,7 @@ describe('Transferências', () => {
 
                 const token = respostaLogin.body.token;
 
-                const resposta = await request('http://localhost:3000')
+                const resposta = await request(baseUrl)
                     .post('/transferencia')
                     .set('Content-Type', 'application/json')
                     .set('Authorization', 'Bearer ' + token)
